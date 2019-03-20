ada4d51c5bc05d7fe2b892409132a5b601261c2f
Rony <ronymmoura@gmail.com>
2019-03-20 16:05:48 -0300

    Criando home de assistidos

--- src/pages/Home/HomeAssistido.tsx
+++ src/pages/Home/HomeAssistido.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { PlanoVinculadoService, FichaSalarioContribuicaoService, FichaContribPrevidencialService } from "@intechprev/ps-web-service";
+import { PlanoVinculadoService, ProcessoBeneficioService, FichaFinancAssistidoService } from "@intechprev/ps-web-service";
 
 import { Row, Col, Box, CampoEstatico, TipoCampoEstatico } from "@intechprev/componentes-web";
 import { HomeCard } from "./HomeCard";
@@ -10,6 +10,8 @@ interface Props { }
 
 interface State {
     plano: any;
+    processo: any;
+    ficha: Array<any>;
 }
 
 export class HomeAssistido extends React.Component<Props, State> {
@@ -20,7 +22,9 @@ export class HomeAssistido extends React.Component<Props, State> {
         super(props);
 
         this.state = {
-            plano: {}
+            plano: {},
+            processo: {},
+            ficha: []
         }
     }
 
@@ -32,8 +36,10 @@ export class HomeAssistido extends React.Component<Props, State> {
         await this.page.current.loading(true);
 
         var plano = await PlanoVinculadoService.Buscar();
+        var processo = await ProcessoBeneficioService.BuscarPorPlano(plano.SQ_PLANO_PREVIDENCIAL);
+        var ficha = await FichaFinancAssistidoService.BuscarPorProcesso(processo.SQ_PROCESSO);
 
-        await this.setState({ plano });
+        await this.setState({ plano, processo, ficha });
 
         await this.page.current.loading(false);
     }
@@ -47,12 +53,71 @@ export class HomeAssistido extends React.Component<Props, State> {
                             {this.state.plano.DS_PLANO_PREVIDENCIAL}
                         </HomeCard>
                     </Col>
-                    <Col tamanho={"lg-3"}>
+                    <Col>
                         <HomeCard titulo={"Situação"}>
                             {this.state.plano.DS_SIT_PLANO}
                         </HomeCard>
                     </Col>
                 </Row>
+
+                <Row className={"mt-4"}>
+                    <Col>
+                        <HomeCard titulo={"Benefício"}>
+                            {this.state.processo.DS_ESPECIE}
+                        </HomeCard>
+                    </Col>
+                    <Col>
+                        <HomeCard titulo={"Processo"}>
+                            {this.state.processo.NR_PROCESSO}/{this.state.processo.NR_ANO_PROCESSO}
+                        </HomeCard>
+                    </Col>
+                </Row>
+
+                <Row className={"mt-4"}>
+                    <Col>
+                        <HomeCard titulo={"Data Concessão"}>
+                            {this.state.processo.DT_CONCESSAO}
+                        </HomeCard>
+                    </Col>
+                    <Col>
+                        <HomeCard titulo={"Situação"}>
+                            {this.state.processo.DS_MOT_SITUACAO}
+                        </HomeCard>
+                    </Col>
+                </Row>
+
+                {this.page.current &&
+                    <Row className={"mt-4"}>
+                        <Col>
+                            <Box titulo={"Última Folha "} label={"competência " + this.state.ficha[0].DT_COMPETENCIA}>
+                                
+                                <table className={"table table-striped table-sm"}>
+                                    <thead>
+                                        <tr>
+                                            <th>Rubrica</th>
+                                            <th className={"text-right"}>Tipo</th>
+                                            <th className={"text-right"}>Valor</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {this.state.ficha.map((item, index) => {
+                                            return (
+                                                <tr key={index}>
+                                                    <td>{item.DS_RUBRICA}</td>
+                                                    <td className={"text-right"}>{item.DS_LANCAMENTO}</td>
+                                                    <td className={"text-right"}>
+                                                        <CampoEstatico valor={item.VL_CALCULO} tipo={TipoCampoEstatico.dinheiro} />
+                                                    </td>
+                                                </tr>
+                                            )
+                                        })}
+                                    </tbody>
+                                </table>
+
+                            </Box>
+                        </Col>
+                    </Row>
+                }
             </Page>
         );
     }
